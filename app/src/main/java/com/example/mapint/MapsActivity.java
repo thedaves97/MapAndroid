f8f629c5e37c149a2d24a44b89b5a7382301d2af
Davide Sgaramella <51700197+thedaves97@users.noreply.github.com>
2020-12-14 13:21:12 +0100

    Intermediate commit - Lettura da JSON e posizionamento marker su mappa

--- app/src/main/java/com/example/mapint/MapsActivity.java
+++ app/src/main/java/com/example/mapint/MapsActivity.java
@@ -9,10 +9,18 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    ArrayList<Marker> marker = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,24 +30,88 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        //LETTURA JSON
+        InputStream is = getResources().openRawResource(R.raw.marker);
+        Writer writer = new StringWriter();
+        char[] buffer = new char[1024];
+
+        try {
+            Reader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+
+            int n;
+            while((n = reader.read(buffer)) !=-1)
+            {
+                writer.write(buffer, 0, n);
+            } //FINE WHILE
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String marker_array = writer.toString();
+
+
+        //FETCH JSON
+        try {
+
+            JSONObject json = new JSONObject(marker_array);
+            JSONArray jArray =  json.getJSONArray("markers");
+            for (int i = 0;i<jArray.length();i++)
+            {
+                JSONObject obj = jArray.getJSONObject(i);
+                /*String name = obj.getString("name");
+                String type = obj.getString("type");
+                String add = obj.getString("address");
+                double lat = obj.getDouble("lat");
+                double lon = obj.getDouble("lon");*/
+                Marker m = new Marker();
+                m.setName(obj.getString("name"));
+                m.setType(obj.getString("type"));
+                m.setAddress(obj.getString("address"));
+                m.setLat(obj.getDouble("lat"));
+                m.setLon(obj.getDouble("lon"));
+                marker.add(m);
+
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        String name, type, add;
+        LatLng pos;
+        double lat;
+        double lon;
+
+        /*
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+         */
+        //CREAZIONE MARKER
+        for (Marker m: marker)
+        {
+            lat = m.getLat();
+            lon = m.getLon();
+
+            pos = new LatLng(lat, lon);
+            mMap.addMarker(new MarkerOptions().position(pos).title("Name: " + m.getName() + "\nType: " + m.getType() + "\nAddress: " + m.getAddress()));
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(15.0f));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(pos));
+        }
+
+
     }
 }
